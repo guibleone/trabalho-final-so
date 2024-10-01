9211d4a70fd1410555b637a40e5b3eadbf11aaa4
Guilherme Leone <guilhermebastosleone@gmail.com>
2024-10-01 14:16:10 -0300

    First commit

---
+++ mergesort.c
@@ -0,0 +1,13 @@
+/*
+  Final project for the discipline of Operational Systems
+  at Unicamp. 
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+
+  return 0;
+}
