71a5447572fe56f7d8439c73887686813dd47f17
Guilherme Leone <guilhermebastosleone@gmail.com>
2024-10-24 16:56:37 -0300

    Begining the vesrion 2

--- src/main_funcs.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_FUNCS_H
-#define MAIN_FUNCS_H
-
-int *getNumbers(int argc, char *argv[], int *total_numbers);
-int *getSortedNumbers(int *numbers, unsigned int total_numbers, int n_threads);
-void printSortedNumbers(int *sorted_numbers, unsigned int total_numbers,
-                        const char *output_file);
-
-#endif
